5ddd903bc77c311cb069db48ff34ae062b28a051
HMC-consulting <connect@howellmarine.co.uk>
2025-11-18 18:26:31 +0000

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,26 +1,30 @@
-import base64
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import base64
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 
 
-def send_email(subject, body, creds, email_to):
+def send_email(subject, body_html, body_text, creds: Credentials, email_to):
     """
-    Sends an email using the Gmail API.
+    Sends a multipart (HTML + plain text) email using Gmail API.
     """
 
-    # Gmail API service
     service = build("gmail", "v1", credentials=creds)
 
-    # Create MIME email
-    message = MIMEText(body, "plain")
-    message["to"] = email_to
-    message["subject"] = subject
+    message = MIMEMultipart("alternative")
+    message["To"] = email_to
+    message["Subject"] = subject
 
-    # Encode in base64
-    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    # Add text + HTML versions
+    part1 = MIMEText(body_text, "plain")
+    part2 = MIMEText(body_html, "html")
 
-    # Send the email
+    message.attach(part1)
+    message.attach(part2)
+
+    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode("utf-8")
+
     service.users().messages().send(
         userId="me",
         body={"raw": raw_message}
